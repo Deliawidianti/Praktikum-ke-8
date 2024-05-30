7c4fdde9ddd16addd960e17a71e9a1249334d4b4
Deliawidianti <deliawidianti08@gmail.com>
2024-05-31 01:44:26 +0700

    Add files via upload

---
+++ matrix.py
@@ -0,0 +1,11 @@
+#matrix dengan ukuran 2x2
+matrixA = [[1,0]],[[0,1]]
+print(matrixA)
+
+#matrix dengan ordo 3x3
+matrixB = [[1,0,1], [0,1,0], [1,0,1]]
+print(matrixB)
+
+#matrix dengan ordo 4x4
+matrixC = [[1,0,1,0], [0,1,0,1],[1,0,1,0],[0,1,0,1]]
+print(matrixC)
